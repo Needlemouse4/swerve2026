9a1060aae5f97ba1715aebd4e059b27b554e7fac
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-01-31 11:06:12 -0500

    Vision Code
    all of this vision code should be working and stable.

--- src/main/java/frc/robot/subsystems/ElasticData.java
+++ src/main/java/frc/robot/subsystems/ElasticData.java
@@ -1,7 +1,41 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElasticData extends SubsystemBase{
-    
+    PhotonVision cameraData = new PhotonVision("testingCamera");
+
+    @Override
+    public void periodic(){
+        cameraData.update();
+        boolean targetVisible = cameraData.targetVisible();
+        double[] targetIDs = cameraData.getIDs().stream()
+        .mapToDouble(Double::doubleValue)
+        .toArray();
+
+        SmartDashboard.putNumberArray("Target IDs", targetIDs);
+        SmartDashboard.putBoolean("Target Visible", targetVisible);
+        for(var id : targetIDs){
+            double yaw = cameraData
+            .getTargetYaw((int) id)
+            .orElse(Double.NaN);
+            if (!Double.isNaN(yaw)){
+                SmartDashboard.putNumber("Target" + id + "yaw", yaw);
+            }
+        }
+        for(var id : targetIDs){
+            double pitch = cameraData
+            .getTargetYaw((int) id)
+            .orElse(Double.NaN);
+            if (!Double.isNaN(pitch)){
+                SmartDashboard.putNumber("Target" + id + "pitch", pitch);
+            }
+        }
+    }
+
+
+
+
+
 }
