b3ff21fa2e34d19fa1a6d0a7da3dc604b4f25e6f
Needlemouse4 <1dykstrataz@hdsb.ca>
2026-01-31 10:44:17 -0500

    FirstPush

---
+++ src/main/java/frc/robot/subsystems/practice.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+public class practice {
+    public static final double exp(double exponent) {
+        double num = 3;
+        double exponentialNum = 1;
+        num += exponentialNum;
+        exponentialNum++;
+        return num;
+    }
+}
