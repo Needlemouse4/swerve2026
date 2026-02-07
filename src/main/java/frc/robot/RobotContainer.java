76e79e5fceacd19291deb8efc6101084082db91c
Needlemouse4 <1dykstrataz@hdsb.ca>
2026-02-07 15:41:34 -0500

    Set robotContainer back to the photon version

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,16 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+//test
 package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
-
-import java.io.Console;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -20,6 +19,9 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.OldTunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.ElasticData;
+import frc.robot.subsystems.PhotonVision;
+import frc.robot.subsystems.VisionData;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * OldTunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -27,19 +29,24 @@ public class RobotContainer {
     //MaxSpeed * 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.2).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDeadband(MaxSpeed * 0.2).withRotationalDeadband(MaxAngularRate * 0.2) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
             private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
+    private PhotonVision vision = new PhotonVision("testingCamera");
+
+    private ElasticData elasticData = new ElasticData(logger, vision);
+
+    private final double MaxYaw = 29;
+
+    private final double speedDamper = 3.5;
+
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = OldTunerConstants.createDrivetrain();
-
-    private double speedDamper = 2;
-    private double cooldown = 0.2;
 
     public RobotContainer() {
         configureBindings();
@@ -51,10 +58,11 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX((-joystick.getLeftY() * MaxSpeed) / speedDamper) // Drive forward with negative Y (forward)
-                    .withVelocityY((-joystick.getLeftX() * MaxSpeed) / speedDamper) // Drive left with negative X (left)
-                    .withRotationalRate((-joystick.getRightX() * MaxAngularRate) / speedDamper) // Drive counterclockwise with negative X (left)
-            )
+                drive.withVelocityX((-joystick.getLeftY() * MaxSpeed) / speedDamper) //Drive forward with negative Y (forward)
+                    .withVelocityY(((-vision.getAnyYaw()/MaxYaw) * MaxSpeed) / speedDamper) //Drive left with negative X (left)
+                    .withRotationalRate((1 - (vision.getZRotation()/Math.PI) * MaxAngularRate) / speedDamper) // Don't rotate Drive counterclockwise with negative X (left)
+                    //Zero is a placeholder value
+                )       
         );
 
         // Idle while the robot is disabled. This ensures the configured
@@ -69,15 +77,15 @@ public class RobotContainer {
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
         ));
 
-        //joystick.y().debounce(cooldown);
-        //joystick.y().onTrue(getAutonomousCommand());
-
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
         joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
         joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        joystick.x().onTrue(pipelineSwitcher());
+        //joystick.leftTrigger(0.5).whileTrue(moveAprilTagLeft());
+        //joystick.rightTrigger(0.5).whileTrue(moveAprilTagRight());
 
         // Reset the field-centric heading on left bumper press.
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
@@ -103,9 +111,45 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle)
         );
     }
-    public Command changeDamperCommand() {
-        return Commands.sequence(
-          //Change Damper  
-        );
+    public Command moveAprilTagLeft() {
+        if ((vision.getAnyYaw() <= 10) && (vision.getAnyYaw() >= -10)) {
+            return Commands.sequence(
+                drivetrain.applyRequest(() ->
+                drive.withRotationalRate(-1 * MaxAngularRate) // Drive so that april tag is left 
+                // Note ** prolly change -1 and 1 in the next Command to a variable
+                            )
+            ); //Rotate the robot right
+        } else {
+            return Commands.sequence(); //Do nothing     
+        }
+    }
+    public Command moveAprilTagRight() {
+       if ((vision.getAnyYaw() <= 10) && (vision.getAnyYaw() >= -10)) {
+            return Commands.sequence(
+                
+                drivetrain.applyRequest(() ->
+                drive.withRotationalRate(1 * MaxAngularRate) // Drive so that april tag is right
+                            )
+            ); //Rotate the robot left
+        } else {
+            return Commands.sequence(); //Do nothing     
+        }
+    }
+
+    public Command pipelineSwitcher(){
+        VisionData visionData = new VisionData("testingCamera");
+        return Commands.runOnce(()->{
+            if (visionData.getPipelineMethod() == 0) {
+                visionData.pipelineSwitcher(1);
+                SmartDashboard.putNumber("Pipeline",visionData.getPipelineMethod());
+            }
+            else{
+                visionData.pipelineSwitcher(0);
+                SmartDashboard.putNumber("Pipeline",visionData.getPipelineMethod());
+            }
+        });
+        
+        
+        
     }
 }
